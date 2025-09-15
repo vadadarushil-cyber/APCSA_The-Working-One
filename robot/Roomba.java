f6b130f42d1c279beeb2aba1c3bb08900c9112fd
vadadarushil-cyber <vadadarushil@gmail.com>
2025-09-15 01:56:39 +0000

    IM DONE MRS CYR

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -7,100 +7,115 @@ public class Roomba implements Directions {
     // Main method to make this self-contained
     public static void main(String[] args) {
         // LEAVE THIS ALONE!!!!!!
-        String worldName = "robot/basicRoom.wld";
+        String worldName = "robot/finalTestWorld2024.wld";
 
         Roomba cleaner = new Roomba();
-        int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
-        System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
+        cleaner.cleanRoom(worldName);
     }
 
-    
     private Robot roomba;
 
-    
+    int total_piles = 0;
     int largest_pile = 0;
+    int largest_pile_x = 0;
+    int largest_pile_y = 0;
     int totalBeepers = 0;
+    int totalArea = 0;
 
-    public int cleanRoom(String worldName, int startX, int startY) {
-
-        
-        roomba = new Robot(7, 13, West, 0);
+    public void cleanRoom(String worldName) {
+        // Robot's initial state is now hard-coded as requested.
+        roomba = new Robot(26, 149, West, 0);
 
         World.readWorld(worldName);
         World.setVisible(true);
-		World.setDelay(1);
+        World.setDelay(0);
 
+        boolean done = false;
 
-
-
-
-
-        
-        boolean done = true;
-        //while (!done) means "keep executing the code inside this loop as long as the boolean variable done is false
-        //This is the logical NOT operator. It inverts a boolean value. So, if done is true, !done evaluates to false. If done is false, !done evaluates to true.
-        while (done) {
+        while (!done) {
             clearAndMove();
 
-        
             if (!roomba.frontIsClear()) {
                 if (roomba.facingWest()) {
-                    turnRight(); 
+                    turnRight();
                     if (roomba.frontIsClear()) {
                         roomba.move();
-                        turnRight(); 
+                        turnRight();
                     } else {
-
                         done = true;
                     }
-                } 
-                else if (roomba.facingEast()) {
-                    roomba.turnLeft(); 
+                } else if (roomba.facingEast()) {
+                    roomba.turnLeft();
                     if (roomba.frontIsClear()) {
                         roomba.move();
-                        roomba.turnLeft(); 
+                        roomba.turnLeft();
                     } else {
                         done = true;
                     }
                 }
             }
         }
-        
-        //roomba.turnOff();
-        return totalBeepers;
+
+        roomba.turnOff();
+
+        double average_pile_size = 0.0;
+        if (total_piles > 0) {
+            average_pile_size = (double) totalBeepers / total_piles;
+        }
+
+        double percent_dirty = 0.0;
+        if (totalArea > 0) {
+            percent_dirty = ((double) total_piles / totalArea) * 100;
+        }
+
+        System.out.println("\n--- Cleaning Report ---");
+        System.out.println("Area of the room: " + totalArea + " spaces");
+        System.out.println("Number of piles: " + total_piles);
+        System.out.println("Total number of beepers collected: " + totalBeepers);
+        System.out.println("Largest pile of beepers: " + largest_pile);
+        System.out.println("Location of the largest pile: (" + largest_pile_x + ", " + largest_pile_y + ")");
+        System.out.println("Average pile size: " + String.format("%.2f", average_pile_size) + " beepers");
+        System.out.println("Percent dirty (piles/area): " + String.format("%.2f", percent_dirty) + "%");
+        System.out.println("-----------------------");
     }
 
-   public void clearAndMove() {
-        int beepers = 0;
+    public void clearAndMove() {
+        // First, check for beepers at the current location
+        int pileBeepers = 0;
+        if (roomba.nextToABeeper()) {
+            total_piles++;
+        }
+
         while (roomba.nextToABeeper()) {
             roomba.pickBeeper();
-            totalBeepers++;
-            beepers++;
-        }
-        if (beepers > largest_pile) {
-            largest_pile = beepers;
+            pileBeepers++;
         }
 
-        while (roomba.frontIsClear()) {
+        if (pileBeepers > largest_pile) {
+            largest_pile = pileBeepers;
+            largest_pile_x = roomba.street();
+            largest_pile_y = roomba.avenue();
+        }
+
+        totalBeepers += pileBeepers;
+
+        // Then, move and check for beepers at each new location
+        if (roomba.frontIsClear()) {
             roomba.move();
-            beepers = 0;
-            while (roomba.nextToABeeper()) {
-                roomba.pickBeeper();
-                totalBeepers++;
-                beepers++;
-            }
-            if (beepers > largest_pile) {
-                largest_pile = beepers;
+            totalArea++;
+            
+            // Check for a new pile at the new location
+            if (roomba.nextToABeeper()) {
+                clearAndMove(); // Recursively call to clean the new pile
             }
         }
     }
 
-  public void turnRight() {
-   int i = 0; 
-   while (i < 3) { 
-      roomba.turnLeft(); 
-      i++; 
-   }
-}
+    public void turnRight() {
+        int i = 0;
+        while (i < 3) {
+            roomba.turnLeft();
+            i++;
+        }
+    }
 }
