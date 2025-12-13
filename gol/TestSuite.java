73f92491883f7b84e50b0dcaf2d6f6bdddb1875f
vadadarushil-cyber <vadadarushil@gmail.com>
2025-12-13 04:36:27 +0000

    Finished the Project and passed mrs. Cyr

--- gol/TestSuite.java
+++ gol/TestSuite.java
@@ -10,22 +10,16 @@ public class TestSuite {
 
         GameOfLife board = new GameOfLife(5, 5);
 
-        // Test count neighbors
         int[][] data = {{ 1 }};
 
-        // Set a single cell, then check that it has zero neighbors.
         board.set(2, 2, data);
         pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
 
-        // Set the middle top neighbor.
         board.set(1, 2, data);
         pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
 
-        // Set the bottom middle neighbor.
         board.set(3, 2, data);
         pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
-
-        // Test update state
         board.step();
         board.print();
         pass &= expect(board.get(2,3), 1, "Expect line to rotate");
@@ -33,7 +27,6 @@ public class TestSuite {
         board.step();
         board.print();
         pass &= expect(board.get(1,2), 1, "Expect line to rotate");
-
 
         if (pass == true)
         {
@@ -54,10 +47,4 @@ public class TestSuite {
         System.out.println(comment + ", value: " + input + " not equal to expected: " + expected);
         return false;
     }
-
-    private static boolean testPattern(int[][] input, int[][] expected)
-    {
-        return true;
-    }
-
 }
