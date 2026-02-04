6903d90a5c8acdfffa750a4a6ab44cf30e368fdb
vadadarushil-cyber <vadadarushil@gmail.com>
2026-02-04 01:31:04 +0000

    main

--- sorting/Main.java
+++ sorting/Main.java
@@ -23,8 +23,11 @@ public class Main {
     public static int[] randomArray(int length)
     {
         // TODO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
+        int [] a = new int[length];
+                for (int i = 0; i < a.length; i++) {
+            a[i] = (int)(Math.random() * 1000);
+        }
+
         return a;
     }
-
 }
