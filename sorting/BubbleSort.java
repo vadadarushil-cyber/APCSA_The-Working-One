56e1d798ddc4815ec538b6469e9e46e8f0cc75c9
vadadarushil-cyber <vadadarushil@gmail.com>
2026-02-04 03:27:40 +0000

    DONE

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,20 +2,28 @@ package sorting;
 
 public class BubbleSort implements Sorter {
 
+    public int steps = 0;
+
     public void sort(int[] input)
     {
+        steps = 0;
+
         // Loop for all elements in the array
         for (int outer = 0; outer < input.length - 1; outer++) {
             // Inner loop is one shorter for every outer loop.
             // Each loop ensures that the largest item moves to the end.
             for (int inner = 0; inner < input.length - outer - 1; inner++) {
                 // If the two elements are out of order, swap them.
+                steps++; 
                 if (input[inner] > (input[inner + 1])) {
                     int temp = input[inner];
                     input[inner] = input[inner + 1];
                     input[inner + 1] = temp;
+                    steps++; 
                 }
             }
         }
+
+        System.out.println("Bubble Sort steps: " + steps);
     }
 }
--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -2,68 +2,67 @@ package sorting;
 
 public class MergeSort implements Sorter {
 
+    public int steps = 0;
+
     public void sort(int[] input) {
-        // Start mergeSort by providing the whole array
+        steps = 0;
         mergeSort(input, input.length);
+        System.out.println("Merge Sort steps: " + steps);
     }
-// this is the one i presented on
-     /**
+
+    /**
      *  Description of the Method
      *
      * @param  list  reference to an array of integers to be sorted
      */
-    public void mergeSort(int [] list, int n){
-        if (n < 2)
-        {
+    public void mergeSort(int[] list, int n) {
+        if (n < 2) {
             return;
         }
-        // Split the array into two chunks
+
         int mid = n / 2;
         int[] l = new int[mid];
         int[] r = new int[n - mid];
 
-        // Copy first half to l
-        for (int i = 0; i < mid; i++)
-        {
+        for (int i = 0; i < mid; i++) {
             l[i] = list[i];
+            steps++;
         }
-        // Copy second half to r
-        for (int i = mid; i < n; i++)
-        {
+
+        for (int i = mid; i < n; i++) {
             r[i - mid] = list[i];
+            steps++;
         }
-        // Recursive call to mergeSort on split array
+
         mergeSort(l, mid);
         mergeSort(r, n - mid);
 
-        // merge the separate chunks
         merge(list, l, r, mid, n - mid);
     }
 
-    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-        int i = 0, j = 0, k = 0;
-        // Copy elements from l and r into the output a
-        // Copy the smaller element from each chunk first.
-        while (i < left && j < right)
-        {
-            if (l[i] <= r[j])
-            {
+    public void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left && j < right) {
+            steps++;
+            if (l[i] <= r[j]) {
                 a[k++] = l[i++];
-            }
-            else
-            {
+            } else {
                 a[k++] = r[j++];
             }
+            steps++;
         }
 
-        // Copy remaining elements to the output
-        while (i < left)
-        {
+        while (i < left) {
             a[k++] = l[i++];
+            steps++;
         }
-        while (j < right)
-        {
+
+        while (j < right) {
             a[k++] = r[j++];
+            steps++;
         }
     }
 }
