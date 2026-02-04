25f9aa3209f2616d86b95af9a9361b256556b156
vadadarushil-cyber <vadadarushil@gmail.com>
2026-02-04 03:19:33 +0000

    almost odne

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,18 +1,20 @@
 package sorting;
-
 public class InsertionSort implements Sorter {
-
+    public int steps = 0;
     public void sort(int[] input) {
+        steps = 0;
         for (int i = 1; i < input.length; i++) {
-
-            int key = input[i];    
-            int j = i - 1;           
-
+            int key = input[i];
+            int j = i - 1;
             while (j >= 0 && input[j] > key) {
+                steps++;            
                 input[j + 1] = input[j];
+                steps++;            
                 j--;
             }
             input[j + 1] = key;
+            steps++;                 
         }
+        System.out.println("Insertion Sort steps: " + steps);
     }
 }
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,19 +1,21 @@
 package sorting;
-
 public class SelectionSort implements Sorter {
-
+    public int steps = 0;
     public void sort(int[] input) {
+        steps = 0;
         for (int i = 0; i < input.length - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < input.length; j++) {
+                steps++; 
                 if (input[j] < input[minIndex]) {
                     minIndex = j;
                 }
             }
-
             int temp = input[i];
             input[i] = input[minIndex];
             input[minIndex] = temp;
+            steps++; 
         }
+        System.out.println("Selection Sort steps: " + steps);
     }
 }
