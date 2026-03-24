5fc7dbcbe73193f5f7fe6b82f9408f4093da82a7
vadadarushil-cyber <vadadarushil@gmail.com>
2026-03-24 21:47:49 +0000

    he

--- polygon/Main.java
+++ polygon/Main.java
@@ -1,12 +1,20 @@
 package polygon;
 
+import java.awt.geom.Point2D;
+
 public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+
+        // Add some points so draw() shows something
+        myPolygon.add(new Point2D.Double(0, 0));
+        myPolygon.add(new Point2D.Double(0, 100));
+        myPolygon.add(new Point2D.Double(100, 100));
+        myPolygon.add(new Point2D.Double(100, 0));
+
         myPolygon.draw();
 
         TestSuite.run();
     }
-    
 }
